568bdf1692caa37f32757e4ab40c15e1b854701e
Rajendra Alluri <rajendra.alluri@gmail.com>
2015-04-23 20:42:25 +0530

    initial template of web server pages

---
+++ webserver/js/data.js
@@ -0,0 +1,5 @@
+var tweetData = [{"handle":"rpermaual",
+	"tagInfo": [{"hash":"development", "percentage": 20}, {"hash":"testing", "percentage": 30}]
+}, {"handle":"gvamsikris",
+	"tagInfo": [{"hash":"development", "percentage": 20}, {"hash":"testing", "percentage": 30}]
+}]
